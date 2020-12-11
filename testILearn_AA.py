6da8d2626e89d91507e3e53d544c5ff938302dbe
abramoago <abramoago@gmail.com>
2020-12-11 12:18:19 +0100

    validation of Incremental learning

--- testILearn_AA.py
+++ testILearn_AA.py
@@ -4,28 +4,43 @@ from dl.LocalModelProvider import LocalModelProvider
 import numpy as np
 import pickle
 
-GENERATE_PICKLE = False
+GENERATE_PICKLE = True
 
 model_provider = LocalModelProvider('models')
 segmenter = model_provider.load_model('Thigh')
 
 data_in = np.load('testImages/test_data.npy')
-segment_in = np.load('testImages/test_segment.npz')
+segment_in = np.load('testImages/test_segment.npy',allow_pickle=True)
 
 n_slices = data_in.shape[2]
 resolution = np.array([1.037037, 1.037037])
 
+LABELS_DICT = {
+        1: 'VL',
+        2: 'VM',
+        3: 'VI',
+        4: 'RF',
+        5: 'SAR',
+        6: 'GRA',
+        7: 'AM',
+        8: 'SM',
+        9: 'ST',
+        10: 'BFL',
+        11: 'BFS',
+        12: 'AL'
+    }
+
 if GENERATE_PICKLE:
     print("Generating data")
-    slice_range = range(4,n_slices-5)
+    slice_range = range(4,n_slices-5) # range(15,25)
     image_list = []
     seg_list = []
 
     for slice in slice_range:
         image_list.append(data_in[:,:,slice].squeeze())
         seg_dict = {}
-        for roi_name in segment_in:
-            seg_dict[roi_name] = segment_in[roi_name][:,:,slice]
+        for k, v in LABELS_DICT.items():
+            seg_dict[v] = segment_in[slice][v][:,:] # segment_in[roi_name][:,:,slice]
         seg_list.append(seg_dict)
 
     pickle.dump(seg_list, open('testImages/test_segment.pickle', 'wb'))
---
+++ validation_ILearn.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pickle
+from generate_thigh_model import coscia_unet as unet
+import dl.common.preprocess_train as pretrain
+
+model=unet()
+#model.load_weights('weights/weights_coscia.hdf5') ## old
+model.load_weights('Weights_incremental/thigh/weights -  5 -  86965.35.hdf5') ## incremental
+
+seg_list = pickle.load(open('testImages/test_segment.pickle', 'rb'))
+image_list = pickle.load(open('testImages/test_data.pickle', 'rb'))
+
+LABELS_DICT = {
+        1: 'VL',
+        2: 'VM',
+        3: 'VI',
+        4: 'RF',
+        5: 'SAR',
+        6: 'GRA',
+        7: 'AM',
+        8: 'SM',
+        9: 'ST',
+        10: 'BFL',
+        11: 'BFS',
+        12: 'AL'
+    }
+'''
+LABELS_DICT = {
+        1: 'SOL',
+        2: 'GM',
+        3: 'GL',
+        4: 'TA',
+        5: 'ELD',
+        6: 'PE',
+        }
+'''
+MODEL_RESOLUTION = np.array([1.037037, 1.037037])
+MODEL_SIZE = (432, 432)
+
+image_list, mask_list = pretrain.common_input_process(LABELS_DICT, MODEL_RESOLUTION, MODEL_SIZE, {'image_list': image_list, 'resolution': MODEL_RESOLUTION}, seg_list)
+
+ch = mask_list[0].shape[2]
+aggregated_masks = []
+mask_list_no_overlap = []
+for masks in mask_list:
+    agg, new_masks = pretrain.calc_aggregated_masks_and_remove_overlap(masks)
+    aggregated_masks.append(agg)
+    mask_list_no_overlap.append(new_masks)
+
+for slice_number in range(len(image_list)):
+    img = image_list[slice_number]
+    segmentation = model.predict(np.expand_dims(np.stack([img,np.zeros(MODEL_SIZE)],axis=-1),axis=0))
+    segmentationnum = np.argmax(np.squeeze(segmentation[0,:,:,:ch]), axis=2)
+    cateseg=np.zeros((432,432,ch),dtype='float32')
+    for i in range(432):
+        for j in range(432):
+            cateseg[i,j,int(segmentationnum[i,j])]=1.0
+    acc=0
+    y_pred=cateseg
+    y_true=mask_list_no_overlap[slice_number]
+    for j in range(ch):  ## Dice
+        elements_per_class=y_true[:,:,j].sum()
+        predicted_per_class=y_pred[:,:,j].sum()
+        intersection=(np.multiply(y_pred[:,:,j],y_true[:,:,j])).sum()
+        intersection=2.0*intersection
+        union=elements_per_class+predicted_per_class
+        acc+=intersection/(union+0.000001)
+    acc=acc/ch
+    print(str(slice_number)+'__'+str(acc))
