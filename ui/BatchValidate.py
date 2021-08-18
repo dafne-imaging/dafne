c07335470c857b66aae2b54f4ff05346781db7b3
fsantini <francesco.santini@gmail.com>
2021-08-18 12:12:24 +0200

    Batch validation app

--- ui/BatchValidate.py
+++ ui/BatchValidate.py
@@ -1,21 +1,29 @@
 #  Copyright (c) 2021 Dafne-Imaging Team
 import os
 import sys
+import traceback
 
 import numpy as np
 from PyQt5.QtCore import pyqtSignal, pyqtSlot
-from PyQt5.QtWidgets import QWidget, QMessageBox, QFileDialog, QApplication, QMainWindow
+from PyQt5.QtWidgets import QWidget, QMessageBox, QFileDialog, QApplication, QMainWindow, QInputDialog
 
 from config import config
+from dl.RemoteModelProvider import RemoteModelProvider
+from dl.misc import calc_dice_score
 from ui import GenericInputDialog
 from ui.ValidateUI import Ui_ValidateUI
 from ui.pyDicomView import ImListProxy
 from utils.ThreadHelpers import separate_thread_decorator
 from utils.dicomUtils.misc import dosma_volume_from_path
 
+CLASSIFICATION = 'Leg'
+TIMESTAMP_INTERVAL = (1625471255, 1625471255) #(1625471255, 1626096769)
+UPLOAD_STATS = True
+
 
 class BatchValidateWindow(QWidget, Ui_ValidateUI):
-    update_progress = pyqtSignal(int, int)
+    update_progress = pyqtSignal(int, int, str)
+    update_overall_progress = pyqtSignal(int, int)
     alert_signal = pyqtSignal(str)
     success = pyqtSignal()
 
@@ -24,29 +32,61 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
         self.setupUi(self)
         self.setWindowTitle("Batch model validation")
         self.update_progress.connect(self.set_progress)
+        self.update_overall_progress.connect(self.set_overall_progress)
         self.alert_signal.connect(self.alert)
         self.data_choose_Button.clicked.connect(self.choose_data)
+        self.mask_choose_Button.clicked.connect(self.choose_mask_dir)
         self.resolution = [1,1,1]
         self.resolution_valid = False
         self.medical_volume = None
-        self.imList = []
+        self.im_list = []
         self.basepath = ''
         self.basename = ''
         self.maskpath = ''
         self.mask_list = {}
 
-
-        self.evaluate_Button.clicked.connect(self.calculate)
+        self.evaluate_Button.clicked.connect(self.start_calculation)
 
         self.data = None
+        self.model_provider = None
+        config.load_config()
+        self.init_model_provider()
 
-    def signal_progress(self, value, maximum):
-        self.update_progress.emit(value, maximum)
+    def init_model_provider(self):
+        self.model_provider = RemoteModelProvider(config.GlobalConfig['MODEL_PATH'], config.GlobalConfig['SERVER_URL'],
+                                             config.GlobalConfig['API_KEY'], config.GlobalConfig['TEMP_UPLOAD_DIR'])
 
-    @pyqtSlot(int, int)
-    def set_progress(self, value, maximum):
+        try:
+            available_models = self.model_provider.available_models()
+        except:
+            traceback.print_exc()
+            self.signal_alert("Error connecting to server")
+            sys.exit(-1)
+
+        if CLASSIFICATION not in available_models:
+            self.signal_alert("Model not found")
+            sys.exit(-1)
+
+        self.model_provider.log('Test')
+
+        print("Connection established")
+
+    def signal_progress(self, value, maximum, msg=''):
+        self.update_progress.emit(value, maximum, msg)
+
+    @pyqtSlot(int, int, str)
+    def set_progress(self, value, maximum, msg):
         self.slice_progressBar.setMaximum(maximum)
         self.slice_progressBar.setValue(value)
+        self.status_Label.setText(msg)
+
+    def signal_overall_progress(self, value, maximum):
+        self.update_overall_progress.emit(value, maximum)
+
+    @pyqtSlot(int, int)
+    def set_overall_progress(self, value, maximum):
+        self.overall_progressBar.setMaximum(maximum)
+        self.overall_progressBar.setValue(value)
 
     def signal_alert(self, msg):
         self.alert_signal.emit(msg)
@@ -65,13 +105,18 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
         dataFile, _ = QFileDialog.getOpenFileName(self, caption='Select dataset to import',
                                                   filter=filter)
         if dataFile:
+            self.im_list = []
+            self.mask_list = {}
+            self.mask_choose_Button.setEnabled(False)
+            self.data_location_Text.setText("")
+            self.mask_location_Text.setText("")
             self.load_directory(dataFile)
 
     def loadNumpyArray(self, data):
         if np.max(data.flat) <= 10: data *= 100
         # print data.shape
         for sl in range(data.shape[2]):
-            self.imList.append(data[:, :, sl])
+            self.im_list.append(data[:, :, sl])
 
     def load_dosma_volume(self, medical_volume):
         if np.max(medical_volume.volume) < 10:
@@ -82,12 +127,12 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
         self.medical_volume = medical_volume
         self.resolution = np.array(self.medical_volume.pixel_spacing)
         self.resolution_valid = True
-        self.imList = ImListProxy(self.medical_volume)
+        self.im_list = ImListProxy(self.medical_volume)
 
 
     # load a whole directory of dicom files
     def loadDirectory_internal(self, path):
-        self.imList = []
+        self.im_list = []
         self.resolution_valid = False
         self.resolution = [1, 1, 1]
 
@@ -99,13 +144,13 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
         self.basename = basename
         self.basepath = basepath
 
-        if len(self.imList) > 0:
+        if len(self.im_list) > 0:
             self.curImage = 0
 
     @separate_thread_decorator
-    def load_directory(self, path, override_class=None):
-        self.signal_progress(0, 1)
-        self.imList = []
+    def load_directory(self, path):
+        self.signal_progress(0, 1, "Loading data")
+        self.im_list = []
         self.mask_list = {}
         _, ext = os.path.splitext(path)
         mask_dictionary = None
@@ -134,7 +179,8 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
                     mask_dictionary[mask_name] = bundle[key]
                     print('Found mask', mask_name)
         else:
-            self.loadDirectory_internal(self, path)
+            self.loadDirectory_internal(path)
+
 
         # ask for resolution to be inserted
         if not self.resolution_valid:
@@ -147,10 +193,14 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
                 self.resolution = [output[0], output[1], output[2]]
                 self.resolution_valid = True
 
+        self.data_location_Text.setText(self.basepath)
+        self.mask_choose_Button.setEnabled(True)
+
         if mask_dictionary:
             self.setSplash(True, 1, 2, "Loading masks")
             self.mask_location_Text.setText(self.basename)
             self.masksToRois(mask_dictionary, 0)
+            self.mask_choose_Button.setEnabled(False)
         self.signal_progress(0, 1)
 
         if self.mask_location_Text.text() != '':
@@ -212,7 +262,7 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
         basename = os.path.basename(path)
         is3D = False
 
-        self.signal_progress(0, 2)
+        self.signal_progress(0, 2, "Loading masks")
 
         def fail(text):
             self.signal_progress(0, 2)
@@ -221,14 +271,14 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
         def load_mask_validate(name, mask):
             if name.lower().endswith('.nii'):
                 name = name[:-4]
-            if mask.shape[0] != self.image.shape[0] or mask.shape[1] != self.image.shape[1]:
+            if mask.shape[0] != self.im_list[0].shape[0] or mask.shape[1] != self.im_list[0].shape[1]:
                 print("Mask shape", mask.shape, "self image shape", self.image.shape)
                 fail("Mask size mismatch")
                 return
             if mask.ndim > 2:
                 is3D = True
-                if mask.shape[2] != len(self.imList):
-                    print("Mask shape", mask.shape, "self length", len(self.imList))
+                if mask.shape[2] != len(self.im_list):
+                    print("Mask shape", mask.shape, "self length", len(self.im_list))
                     fail("Mask size mismatch")
                     return
             mask = mask > 0
@@ -239,7 +289,7 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
         if ext in npy_ext:
             mask = np.load(path)
             name = basename
-            self.signal_progress(1, 2)
+            self.signal_progress(1, 2, "Loading masks")
             load_mask_validate(name, mask)
             self.signal_progress(0,1)
             return
@@ -248,7 +298,7 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
             n_masks = len(mask_dict)
             cur_mask = 0
             for name, mask in mask_dict.items():
-                self.signal_progress(cur_mask, n_masks)
+                self.signal_progress(cur_mask, n_masks, "Loading masks")
                 load_mask_validate(name, mask)
             self.signal_progress(0,1)
             return
@@ -258,7 +308,7 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
 
             mask = mask_medical_volume.volume
 
-            self.signal_progress(2, 3)
+            self.signal_progress(2, 3, "Loading masks")
             load_mask_validate(name, mask)
             self.signal_progress(0, 1)
             return
@@ -283,7 +333,7 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
                 dataset[dataset < 1] = 0
                 name, _ = os.path.splitext(os.path.basename(data_path))
 
-                self.signal_progress(2, 3)
+                self.signal_progress(2, 3, "Loading masks")
                 mask = dataset
                 load_mask_validate(name, mask)
                 self.signal_progress(0, 1)
@@ -309,6 +359,82 @@ class BatchValidateWindow(QWidget, Ui_ValidateUI):
                     self.maskToRois2D(name, mask[:, :, sl], sl)
             else:
                 self.maskToRois2D(name, mask, 0)
+
+    @pyqtSlot()
+    def start_calculation(self):
+        comment, ok = QInputDialog.getText(self, "Add comment", "Add an identifier for this dataset")
+        if not ok:
+            return
+        self.calculate(comment)
+
+    @separate_thread_decorator
+    def calculate(self, comment):
+        split_laterality = False
+        print("Imlist len", len(self.im_list))
+        print("Masks:")
+        for key in self.mask_list:
+            print(key, ", ".join(self.mask_list[key].keys()))
+            for roi_name in self.mask_list[key]:
+                if roi_name.endswith('_L'):
+                    split_laterality = True
+
+        print("Split laterality:", split_laterality)
+
+        model_info = self.model_provider.model_details(CLASSIFICATION)
+        timestamps = model_info['timestamps']
+        timestamps_to_download = [timestamp for timestamp in timestamps if TIMESTAMP_INTERVAL[0] <= int(timestamp) <= TIMESTAMP_INTERVAL[1]]
+
+        print("Models to download", timestamps_to_download)
+
+        n_models = len(timestamps_to_download)
+        current_model_n = 0
+
+        for timestamp in timestamps_to_download:
+            self.signal_overall_progress(current_model_n, n_models)
+
+            model = self.model_provider.load_model(CLASSIFICATION,
+                                                  lambda value, maximum: self.signal_progress(value, maximum, f'Downloading {timestamp}'),
+                                                  timestamp=timestamp)
+
+            # perform segmentation
+            n_slices = len(self.mask_list)
+            current_slice = 0
+            dice_scores = []
+            n_voxels = []
+            for slice, mask_dict in self.mask_list.items():
+                self.signal_progress(current_slice, n_slices, f'Evaluating {timestamp}')
+                input_dict = {'image': self.im_list[slice],
+                              'classification': CLASSIFICATION,
+                              'resolution': self.resolution[0:2],
+                              'split_laterality': split_laterality}
+
+                output_masks = model.apply(input_dict)
+                for mask_name, mask in mask_dict.items():
+                    if mask_name in output_masks:
+                        n_vox = mask.sum()
+                        dice = calc_dice_score(mask, output_masks[mask_name])
+                        n_voxels.append(n_vox)
+                        dice_scores.append(dice)
+                        print('Slice', slice, 'Mask', mask_name, 'N', n_vox, 'Dice', dice)
+                current_slice += 1
+
+            dice_scores = np.array(dice_scores)
+            n_voxels = np.array(n_voxels)
+            # print(diceScores)
+            if np.sum(n_voxels) == 0:
+                average_dice = -1.0
+            else:
+                average_dice = np.average(dice_scores, weights=n_voxels)
+
+            message = f'Average dice - {comment} - model {timestamp}: {average_dice}'
+            print(message)
+            if UPLOAD_STATS:
+                self.model_provider.log(message)
+
+            current_model_n += 1
+
+        self.signal_overall_progress(0,1)
+        self.signal_progress(0,1,'Finished')
 
 
 def run():
