6a97e94053c4c32a00ba4de5ec66d86f0cf5ddfa
Francesco Santini <francesco.santini@gmail.com>
2022-06-02 15:42:02 +0200

    Fix capitalization for Mac

--- create_mac_installer.sh
+++ create_mac_installer.sh
@@ -10,10 +10,10 @@ echo $VERSION
 pyinstaller dafne_mac.spec --noconfirm
 cd dist
 echo "Signing code"
-codesign --deep -s "Francesco" dafne.app
+codesign --deep -s "Francesco" Dafne.app
 echo "Creating DMG"
-create-dmg --volname "Dafne" --volicon dafne.app/Contents/Resources/dafne_icon.icns \
-	 --eula dafne.app/Contents/Resources/LICENSE --background ../mac_installer_bg.png \
+create-dmg --volname "Dafne" --volicon Dafne.app/Contents/Resources/dafne_icon.icns \
+	 --eula Dafne.app/Contents/Resources/LICENSE --background ../mac_installer_bg.png \
 	 --window-size 420 220 --icon-size 64 --icon dafne.app 46 31 \
 	 --app-drop-link 236 90 "dafne_$VERSION.dmg" dafne.app
 #rm /tmp/dafne.dmg
